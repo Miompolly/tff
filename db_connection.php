3aceeb97a515fc24b8ab2d4e2759f9aaf7861ade
Miompolly <57joel39@gmail.com>
2025-03-10 16:14:39 +0200

    db

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,9 @@
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "Yawhey@123";
 $database = "tff";
-$port = 3306; // Use the correct port, default is 3306 for MySQL
+$port = 3306;  // Use the correct port, default is 3306 for MySQL
 
 // Enable error reporting for debugging
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
@@ -12,9 +13,10 @@ $conn = new mysqli($servername, $username, $password, $database, $port);
 
 // Check connection
 if ($conn->connect_error) {
-// Display an error message if the connection fails
-die("Connection failed: " . $conn->connect_error);
+    // Display an error message if the connection fails
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // Set charset to utf8mb4 (for handling Unicode characters)
 $conn->set_charset("utf8mb4");
+?>
